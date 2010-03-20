050197e1d2b9b51d093d27d326b8949be4fd5794
Michael Pilgermann <kichkasch@gmx.de>
2010-03-20 12:29:47 +0100

    basics prepared for PHP frontend - design from OSWD imported

---
+++ frontend/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>JuMiCycling</title>
+<meta name="Description" content="Repository for Cycling statistics" />
+<meta name="Keywords" content="cycling workout statistics repository" />
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+<body>
+<div id="wrap">
+  <div id="header">
+    <h1>JuMiCycling</h1>
+    <h2>Repository for Cycling statistics</h2>
+  </div>
+  <div id="menu"> <!--<a href="#">Home</a> <a href="#">About </a> <a href="#">Contact</a>--></div>
+  
+  <div id="content">
+    <h2>Add workout</h2>
+    <p>Coming very soon ...</p>
+    <p>&nbsp;</p>
+    
+    <h2>Past workouts</h2>
+    <p>Table here soon...</p>
+    <p>&nbsp;</p>
+
+  </div>
+  <div id="footer">Content by <a href="mailto:kichkasch@gmx.de">Michael Pilgermann</a> | Open Source Design Template sponsored by <a href="http://www.cmgtechnologies.com/">CMG Technologies</a><a href="#"></a>. </div>
+</div>
+</body>
+</html>
