d3e8d5ec382918c71a4dc3a838ab579f97a836f2
Michael Pilgermann <kichkasch@gmx.de>
2012-07-03 22:06:19 +0200

    few testing

--- frontend/mobWorkoutDetails.php
+++ frontend/mobWorkoutDetails.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-        <title>JuMi Cycling Mobile</title>
+        <title>JuMiCycling Mobile</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" />
         <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
